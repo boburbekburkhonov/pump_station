5ec301c214459fffdac6a35a76f293f69f4f7415
boburbekburkhonov <zafaroviccc@gmail.com>
2025-04-16 16:37:03 +0500

    feat: .

--- src/redux/actions/organizationActions.jsx
+++ src/redux/actions/organizationActions.jsx
@@ -5,18 +5,24 @@ import { GLOBALTYPES } from "./globalTypes";
 
 export const ORGANIZATION_TYPES = {
   GET_ALL_ORGANIZATION_DATA: "GET_ALL_ORGANIZATION_DATA",
+  GET_ALL_ORGANIZATION_DATA_FOR_ORG: "GET_ALL_ORGANIZATION_DATA_FOR_ORG",
 };
 
-export const getAllOrganizationsData = (lang, token) => async (dispatch) => {
+export const getAllOrganizationsData = (lang, token, page, perPage) => async (dispatch) => {
   try {
     const res = await getDataApi(
-      `organizations/getAll?page=1&perPage=10000&lang=${lang}`,
+      `organizations/getAll?page=${page != undefined ? page : "1"}&perPage=${perPage != undefined ? perPage : "10000"}&lang=${lang}`,
       token
     );
 
     dispatch({
       type: ORGANIZATION_TYPES.GET_ALL_ORGANIZATION_DATA,
       payload: res.data.data.data,
+    });
+
+    dispatch({
+      type: ORGANIZATION_TYPES.GET_ALL_ORGANIZATION_DATA_FOR_ORG,
+      payload: res.data.data,
     });
   } catch (err) {
     if (!err.response) {
--- src/redux/reducers/organizationReducers.jsx
+++ src/redux/reducers/organizationReducers.jsx
@@ -4,6 +4,7 @@ import { ORGANIZATION_TYPES } from "../actions/organizationActions";
 
 const initialState = {
   organizations: [],
+  organizationsForOrg: [],
 };
 
 const districtReducer = (state = initialState, action) => {
@@ -12,6 +13,11 @@ const districtReducer = (state = initialState, action) => {
       return {
         ...state,
         organizations: action.payload,
+      };
+    case ORGANIZATION_TYPES.GET_ALL_ORGANIZATION_DATA_FOR_ORG:
+      return {
+        ...state,
+        organizationsForOrg: action.payload,
       };
     default:
       return state;
